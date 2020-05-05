12c34f85767843b76dd09eb433c52867cf85db1e
benjas <909336740@qq.com>
2020-05-05 09:52:11 +0800

    Create RPC-TCP（跨语言）

---
+++ 04-微服务/RPC-TCP/rpcServer/server.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+// golang实现RPC程序，实现求矩形面积和周长
+
+// 1.定义结构体，用于绑定方法
+// 矩形对象，首字母大写
+type Rect struct {
+}
+
+// 2.声明参数的结构体（也可以声明返回值的结构体，因为这里只返回一个值，所以不需要做结构体）
+// 首字母大写，参数首字母也大写
+type Params struct {
+	Width, Height int
+}
+
+// 求矩形面积
+// (r *Rect)：绑定到矩形对象上，Area：方法名（首字母大写），p:参数对象，Params：参数内容
+// ret:返回值对象，*int:返回int类型，参数必须是指针类型，error：必须有error返回声明
+func (r *Rect) Area(p Params, ret *int) error {
+	// 面积的计算公式，上面声明的是指针，那么赋值的也需要是指针
+	*ret = p.Width * p.Height
+	// 如上面定义，ret是一定回返回，error是有问题则返回，那么没问题就返回nil，或者你自己定义一个正确时的返回标识
+	return nil
+}
+
+// 求周长
+// 如上解释一样
+func (r *Rect) Perimeter(p Params, ret *int) error {
+	*ret = (p.Width + p.Height) * 2
+	return nil
+}
+
+// 如果是跨语言，jsonrpc库是基于tcp的，暂不支持http，所以把上面的HTTP注释掉
+func main() {
+	// 1.注册服务
+	_ = rpc.Register(new(Rect))
+	// 2.监听服务
+	listen, err := net.Listen("tcp", ":8000")
+	// 处理异常
+	// 处理下异常
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 3.循环监听服务
+	for {
+		// Accept()会返回一个连接，和它连接的客户端的连接
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		// 匿名函数开一个协程，每来一个客户端就开一个新的
+		// conn net.Conn：声明类型
+		go func(conn net.Conn) {
+			fmt.Println("new client")
+			// 由于是jsonrpc的，所以用jsonrpc.ServeConn去连接
+			jsonrpc.ServeConn(conn)
+		}(conn) // 这个conn才是上面Accept()返回的conn，这个是匿名函数写法
+	}
+	// 跨语言rpc模式流程：注册服务——监听端口——循环监听服务——接收到连接
+	// ——开协程处理多个连接——用jsonrpc.ServeConn去处理发过来的连接
+}
