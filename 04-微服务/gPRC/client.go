29394f287bbeb40e948c2ee9e644a39aff3aa65f
benjas <909336740@qq.com>
2020-05-07 08:12:54 +0800

    Update client.go

--- 04-微服务/gPRC/client.go
+++ 04-微服务/gPRC/client.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main()  {
-	// 1.创建与gPRC服务端的连接
+	// 1.创建与gPRC服务端的连接，grpc.WithInsecure固定搭配
 	conn, e := grpc.Dial("127.0.0.1:8000", grpc.WithInsecure())
 	if e != nil{
 		fmt.Printf("连接异常：%s\n",e)
@@ -20,7 +20,7 @@ func main()  {
 	// 3.组装参数，初始化一个指向类型的指针(*T)
 	req := new(pb.UserRequest)
 	req.Name = "ben"
-	// 4.调用接口并传入参数
+	// 4.调用接口并传入参数，context.Background()固定搭配
 	response, e := client.GetUserInfo(context.Background(), req)
 	if e != nil{
 		fmt.Printf("响应异常%s\n", e)
